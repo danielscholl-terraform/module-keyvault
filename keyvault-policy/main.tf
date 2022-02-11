db656997d9ac802c2fb061938b5e76ee55215903
danielscholl <dascholl@microsoft.com>
2022-02-11 07:42:25 -0600

    Update main.tf

--- keyvault-policy/main.tf
+++ keyvault-policy/main.tf
@@ -12,4 +12,5 @@ resource "azurerm_key_vault_access_policy" "main" {
   key_permissions         = var.key_permissions
   secret_permissions      = var.secret_permissions
   certificate_permissions = var.certificate_permissions
+  storage_permissions     = var.storage_permissions
 }
