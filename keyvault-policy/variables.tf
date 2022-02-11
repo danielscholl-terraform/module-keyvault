2a2cba53e77a20051ababdcfbf720d7b511e94e6
danielscholl <dascholl@microsoft.com>
2022-02-11 07:47:32 -0600

    Update variables.tf

--- keyvault-policy/variables.tf
+++ keyvault-policy/variables.tf
@@ -18,19 +18,25 @@ variable "object_ids" {
 }
 
 variable "key_permissions" {
-  description = "List of key permissions, must be one or more from the following: backup, create, decrypt, delete, encrypt, get, import, list, purge, recover, restore, sign, unwrapKey, update, verify and wrapKey"
+  description = "List of key permissions, must be one or more from the following: Backup, Create, Decrypt, Delete, Encrypt, Get, Import, List, PUrge, Recover, Restore, Sign, UnwrapKey, Update, Verify and WrapKey"
   type        = list(string)
   default     = ["Create", "Delete", "Get", "List"]
 }
 
 variable "secret_permissions" {
   type        = list(string)
-  description = "List of secret permissions, must be one or more from the following: backup, delete, get, list, purge, recover, restore and set."
+  description = "List of secret permissions, must be one or more from the following: Backup, Delete, Get, List, Purge, Recover, Restore and Set."
   default     = ["Delete", "Get", "Set", "List"]
 }
 
 variable "certificate_permissions" {
   type        = list(string)
-  description = "List of storage permissions, must be one or more from the following: backup, delete, deletesas, get, getsas, list, listsas, purge, recover, regeneratekey, restore, set, setsas and update."
+  description = "List of certificate permissions, must be one or more from the following: Backup, Create, Decrypt, Delete, DeleteIssuers, Get, GetIssuers, Import, List, ListIssuers, ManageContacts, Manag Issuers, Purge, Recover, Restore, SetIssuers and Update."
   default     = ["Create", "Delete", "Get", "List"]
+}
+
+variable "storage_permissions" {
+  type        = list(string)
+  description = "List of storage permissions, must be one or more from the following: Backup, Delete, DeleteSAS, Get, GetSAS, List, ListSAS, Purge, Recover, RegenerateKey, Restore, Set, SetSAS and Update."
+  default     = []
 }
